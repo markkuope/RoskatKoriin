f3224e15d3f13ad710b9c64b54a0b8cfe57ea9df
markkuope <72963505+markkuope@users.noreply.github.com>
2022-01-19 17:33:39 +0200

    koodia

---
+++ Assets/Scripts/RoskaSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoskaSpawner : MonoBehaviour
+{
+    [SerializeField]
+    float maxX;  // spawnausalueen max X arvo
+
+    public GameObject[] Roskat; // Taulukko, johon pistet‰‰n peliss‰ k‰ytetyt roskat
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnRoska();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRoska()
+    {
+        int rand = Random.Range(0, Roskat.Length);
+        //Random.Range antaa satunnaisen numeron v‰lill‰ 0 ja Roskat.Length  
+        // Roskat.Length  on taulukon koko
+        //satunnainen numero talletaan rand ñ muuttujaan
+
+        Instantiate(Roskat[rand], transform.position, transform.rotation);
+        //  Instantiate(Roskat[rand],  synnytt‰‰  satunnaisen roskan 
+        //  roska syntyy paikkaan transform.position
+        // roskan rotaatio on transform.rotation
+
+    }
+
+
+}
